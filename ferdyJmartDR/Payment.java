6d9b65faa03bc44090feac28fa5f67e214f88766
Mochamad Ferdy Fauzan <mochamad.ferdy@ui.ac.id>
2021-10-11 21:01:14 +0700

    PT Modul5

--- ferdyJmartDR/Payment.java
+++ ferdyJmartDR/Payment.java
@@ -7,16 +7,16 @@ package ferdyJmartDR;
  * @Mochamad Ferdy Fauzan
  * @11-10-2021
  */
-public class Payment
+public class Payment extends Invoice
 {
     public int productCount;
     public Shipment shipment;
-/*
+
     public Payment(int buyerId, int productId, int productCount, Shipment shipment) {
+    	super(buyerId, productId);
     	this.buyerId = buyerId;
         this.productId = productId;
         this.productCount = productCount;
         this.shipment = shipment;
     }
-    */
 }
