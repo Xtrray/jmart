2d056d7cfb8462bb5a0ab1b6d895f0f0da5e9f2f
Mochamad Ferdy Fauzan <mochamad.ferdy@ui.ac.id>
2021-11-05 14:00:57 +0700

    TP Modul6

--- ferdyJmartDR/Jmart.java
+++ ferdyJmartDR/Jmart.java
@@ -5,18 +5,42 @@ package ferdyJmartDR;
  * Write a description of class Jmart here.
  *
  * @Mochamad Ferdy Fauzan
- * @09-10-2021
+ * @05-11-2021
  */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
+
 public class Jmart
 {
     public void Jmart()
     {
         
     }
-
+    
+    class Country{
+    	public String name;
+    	public int population;
+    	public List<String> listOfStates;
+    }
+    
     public static void main(String[] args)
     {
-    	
+    	String filepath = "C://Java//jmart/city.json";
+    	Gson gson = new Gson();
+    	try {
+    		BufferedReader br = new BufferedReader(new FileReader(filepath));
+    		Country input = gson.fromJson(br, Country.class);
+    		System.out.println("name: " + input.name);
+    		System.out.println("population: " + input.population);
+    		System.out.println("States:");
+    		input.listOfStates.forEach(state -> System.out.println(state));
+    	}
+    	catch(IOException e){
+    		e.printStackTrace();
+    	}
     }
     /*
     public static int getPromo(){
